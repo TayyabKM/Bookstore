69b54b63fa4046fdf8457aab2573ff0561123b72
TayyabKM <109720981+TayyabKM@users.noreply.github.com>
2023-03-09 14:30:35 +0100

    update files

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,17 +1,22 @@
-/* eslint-disable no-param-reassign */
-import { createSlice } from '@reduxjs/toolkit';
+// Actions
+const CHECK_STATUS = 'bookstore-1/categories/CREATE';
 
-const categoriesSlice = createSlice({
-  name: 'categories',
-  initialState: {
-    status: 'Under Construction',
-  },
-  reducers: {
-    checkStatus: (state) => {
-      state.status = 'Under Construction';
-    },
-  },
+// Reducer
+export const categoriesReducer = (state = [], action) => {
+  switch (action.type) {
+    case CHECK_STATUS:
+      return [
+        ...state,
+        action.text,
+      ];
+
+    default:
+      return state;
+  }
+};
+
+// Action Creators
+export const checkStatus = () => ({
+  type: CHECK_STATUS,
+  text: 'Under Construction',
 });
-
-export const { checkStatus } = categoriesSlice.actions;
-export default categoriesSlice.reducer;
